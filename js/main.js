6e5128d74bd16df1c0a994796a0cdc085ce235f6
artemf92 <57588157+artemf92@users.noreply.github.com>
2020-02-01 17:02:31 +0200

    main.js
    Появление кнопки "Посмотреть" при наведении

--- js/main.js
+++ js/main.js
@@ -1,59 +1,21 @@
 $(document).ready(function() {
-	var	openPopupWD = document.querySelector('.webdevPopup'),
-		openPopupGr = document.querySelector('.groomingPopup'),
-		openPopupPortfolio = document.querySelector('.portfolioPopup');
-	
-	/* Call plugins hrere ...*/
+	let portfolioItems = document.querySelectorAll('.owl-item'); //Элементы корусели
 
-        //ID блока с ссылками #prime_nav
-	$("#nav").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
- 
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
- 
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
 
-	// Grooming maket
 
-	$('#grooming').on('click', function (event) {
-		event.preventDefault();
-		openPopupGr.classList.add('modal--show');
-		console.log('popup');
-	});
-	// WebDev maket
+// Появление кнопки "Посмотреть" при наведении на элемент
 
-	$('#webdev').on('click', function (event) {
-		event.preventDefault();
-		
-		
-		openPopupWD.classList.add('modal--show');
-	});
+	for (let i = 0; i < portfolioItems.length; i++) {
+		portfolioItem = portfolioItems[i];
+		let btnShow = portfolioItem.querySelector('#btnShow');
 
-	// Макет портфолио
+		portfolioItem.addEventListener('mouseover', function () {
+		btnShow.classList.add('unhidden');
+		});
 
-	$('#portfolioMaket').on('click', function (event) {
-		event.preventDefault();
-		
-		
-		openPopupPortfolio.classList.add('modal--show');
-	});
-
-	// Закрытие макетов
-
-	$('.modal__button').on('click', function() {
-
-		// var closePopup = document.querySelector('.modal');
-		openPopupWD.classList.remove('modal--show');
-		openPopupGr.classList.remove('modal--show');
-		openPopupPortfolio.classList.remove('modal--show');
-
-	});
+		portfolioItem.addEventListener('mouseout', function() {
+			btnShow.classList.remove('unhidden');
+		})
+	}
 	
 });
