ec38b61f259bd5699425ab033233a54576138abc
Artem Frolov <57588157+artemf92@users.noreply.github.com>
2020-02-20 09:12:56 +0200

    add smooth scroll to resume

--- js/main.js
+++ js/main.js
@@ -17,5 +17,22 @@ $(document).ready(function() {
 			btnShow.classList.remove('unhidden');
 		})
 	}
-	
+
+
+	// Плавная прокрутка до резюме из шапки
+
+		$("#nav").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+			event.preventDefault();
+ 
+		//забираем идентификатор блока с атрибута href
+			var id  = $(this).attr('href'),
+ 
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
 });
